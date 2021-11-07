a2525408986e677a73827da9565227470624225e
evanMarshallDev <evanmarshall.dev@gmail.com>
2021-11-06 21:07:06 -0300

    Setup components for navbar and footer, using tailwindcss and setup project for eslint/prettier.

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer({}) {
+  return (
+    <footer>
+      <p>
+        Carefully crafted by{' '}
+        <a className='text-blue-600' href='/'>
+          Elysian Web Design
+        </a>
+      </p>
+    </footer>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        <Head>
+          <title>Elysian Web Design</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+
+        <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
+          <h1 className='text-6xl font-bold'>
+            Welcome to{' '}
+            <a className='text-blue-600' href='https://nextjs.org'>
+              Elysian Web Design
+            </a>
+          </h1>
+        </main>
+
+        <Footer />
+      </div>
+    </>
+  );
+}
